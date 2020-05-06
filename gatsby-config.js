e36cd185ed0b84e76be7d38aa46eb51285e17fa0
spaceman <spaceman@pop-os.localdomain>
2020-05-06 09:44:43 -0600

    feat: first changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Garrett Cornwell',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Garrett Cornwell\'s personal website. ',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
